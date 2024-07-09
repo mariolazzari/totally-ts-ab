e94875cfb5e8e74c111423db29672914d4c6082e
Mario Lazzari <mario.lazzari@gmail.com>
2024-07-09 23:06:37 +0200

    infer

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Totally TypeScript by Andrew Burgess
+
+YouTube [playlist](https://www.youtube.com/playlist?list=PL_L_J_Lv0U2owwNFXCoAy3BFPntY2Ry8_)
+
+## Text Adventures in TypeScript
+
+YouTube [video](https://www.youtube.com/watch?v=Jrd1TfZ-KD4&list=PL_L_J_Lv0U2owwNFXCoAy3BFPntY2Ry8_&index=10&t=191s)
+
+## 4 ways to use the TypeScript infer keyword
+
+- Capture the value of a generic argument
+- 
+
+YouTube [video](https://www.youtube.com/watch?v=3Fxoxg_FMpg&t=351s)
+
+
+
+## New **infer** features in TypeScript 4.8
+
+YouTube [video](https://www.youtube.com/watch?v=4eszSJK2LxA&list=PL_L_J_Lv0U2owwNFXCoAy3BFPntY2Ry8_&index=4) 
--- infer.ts
+++ infer.ts
@@ -1,4 +1,5 @@
-// Generics
+// Infer generic type
+
 type QueueJob<Q extends string, P> = {
   queue: Q;
   payload: P;
@@ -17,6 +18,7 @@ type ProcessPayment = {
 type WelcomeEmailJob = QueueJob<"email", WelcomeEmail>;
 type ProcessPaymentJob = QueueJob<"payment", ProcessPayment>;
 
+// pulls out queue name
 type QueueName<J extends QueueJob<string, unknown>> = J extends QueueJob<
   infer N,
   unknown
@@ -27,4 +29,36 @@ type QueueName<J extends QueueJob<string, unknown>> = J extends QueueJob<
 type EmailQueue = QueueName<WelcomeEmailJob>;
 type PaymentQueue = QueueName<ProcessPaymentJob>;
 
+// infer payload type
+type QueuePayload<Q extends QueueJob<string, unknown>> = Q extends QueueJob<
+  string,
+  infer TPayload
+>
+  ? TPayload
+  : never;
+
+type EmailPayload = QueuePayload<WelcomeEmailJob>;
+
 // Arrays
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
+  ? U extends F
+    ? true
+    : Includes<R, U>
+  : never;
+
+// Functions
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...args: infer A
+) => any
+  ? A
+  : never;
+
+type MyReturnType<T> = T extends (args: any[]) => infer R ? R : never;
+
+// Template litterals
+type Chars = " " | "\n" | "\t";
+type Trim<S extends string> = S extends `${Chars}${infer SS}`
+  ? Trim<SS>
+  : S extends `${infer SS}${Chars}`
+  ? Trim<SS>
+  : S;
